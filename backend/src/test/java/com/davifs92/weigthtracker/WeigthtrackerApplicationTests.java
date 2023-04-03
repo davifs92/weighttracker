c926c1567addf5bc33d66e2232c67ef0ab715c73
David Santos <dfirmo-dos-santos@myseneca.ca>
2023-04-02 20:11:07 -0400

    Spring project created

---
+++ backend/src/test/java/com/davifs92/weigthtracker/WeigthtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davifs92.weigthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeigthtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
